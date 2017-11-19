902318755c2804050550777412503315b5faa4a8
feveregg <wk45@afsconnect2.njit.edu>
2017-11-19 18:19:35 -0500

    set connection information

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
 <?php
 
-//Bug Report
+//turn on debugging messages
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
 
+define('DATABASE', 'wk45');
+define('USERNAME', 'wk45');
+define('PASSWORD', 'AmCb81UGG');
+define('CONNECTION', 'sql1.njit.edu');
 
 
 
+       
 
 
+         
 
 ?>
